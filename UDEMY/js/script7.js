7c80439f43b94c88abad8fd96fbe65d8475d51b4
Maxim <example@gmail.com>
2021-06-29 22:21:28 +0400

    Home work and others

--- UDEMY/js/script7.js
+++ UDEMY/js/script7.js
@@ -1,2 +1,27 @@
 "use strict";
 
+// Циклы
+
+let num = 50;
+
+/*while (num <= 55) {
+    console.log(num);
+    num++;
+}*/
+
+console.log("---------------------");
+
+/*do{
+    console.log(num);
+    num++;
+}
+while (num <= 55);*/
+
+for (let i = 0; i <= 10; i++) {
+    if (i === 5) {
+        //break;
+        console.log("-_-");
+        continue;
+    }
+    console.log(i);
+}
